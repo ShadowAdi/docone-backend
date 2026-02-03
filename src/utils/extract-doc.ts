5cfbcdb96664725973417758ea67a89323a1f2e4
ShadowAdi <shadowshukla76@gmail.com>
2026-02-04 03:25:51 +0530

    added exyract doc file

---
+++ src/utils/extract-doc.ts
@@ -0,0 +1,26 @@
+import { logger } from "../config/logger"
+import { AppError } from "./AppError"
+
+export const ExtractedDocument = async (filePath: string) => {
+    const extension = filePath.split(".").pop()?.toLowerCase()
+    try {
+        switch (extension) {
+            case "docx":
+                logger.info(`File Type is doc`)
+                break;
+            case "ppt":
+                logger.info(`File Type is ppt`)
+                break;
+            case 'pdf':
+                logger.info(`File Type is pdf`)
+                break;
+            default:
+                logger.error(`Unsupported file type: ${extension}`)
+                throw new AppError(`Unsupported file type: ${extension}`, 500)
+        }
+    } catch (error) {
+        logger.error(`Failed to extract text from the doc: ${error}`)
+        console.error(`Failed to extract text from the doc: ${error}`)
+        throw new AppError(`Failed to extract text from the doc: ${error}`, 500)
+    }
+}
