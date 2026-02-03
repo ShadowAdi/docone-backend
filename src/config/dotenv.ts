d48e86274e851e0f016ea3253774b35387961111
ShadowAdi <shadowshukla76@gmail.com>
2026-02-04 00:53:13 +0530

    added supabase keys

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -3,6 +3,7 @@ import { configDotenv } from "dotenv";
 configDotenv()
 
 export const PORT = process.env.PORT
-export const DB_URL = process.env.DB_URL
+export const SUPABASE_PROJECT_API_URL = process.env.SUPABASE_PROJECT_API_URL
+export const SUPABASE_PROJECT_API_KEY = process.env.SUPABASE_PROJECT_API_KEY
 export const CLIENT_URL = process.env.CLIENT_URL
 export const NODE_ENV = process.env.NODE_ENV
