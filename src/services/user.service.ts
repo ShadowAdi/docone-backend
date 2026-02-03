f2f9947562034142c4d9a6e576044f19abd63050
ShadowAdi <shadowshukla76@gmail.com>
2026-02-04 01:46:35 +0530

    added create user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,28 @@
+import { eq } from "drizzle-orm"
+import { logger } from "../config/logger"
+import { db } from "../db"
+import { InsertUser, usersTable } from "../db/schema"
+import { AppError } from "../utils/AppError"
+
+export const createUser = async (data: InsertUser) => {
+    try {
+        const getUser = await db.select().from(usersTable).where(eq(usersTable.email, data.email))
+        if (getUser) {
+            logger.error(`User Already Exists with same mail`)
+            console.error(`User Already exists with same mail`)
+            throw new AppError(`User Already exists with same mail`, 400)
+        }
+        const [{ id }] = await db.insert(usersTable).values(data).returning({
+            id: usersTable.id
+        })
+        return {
+            "success": true,
+            "message": "New User Created",
+            id: id
+        }
+    } catch (error) {
+        logger.error(`Failed to create user: ${error}`)
+        console.error(`Failed to create user: ${error}`)
+        throw new AppError(`Failed to Create user`, 500)
+    }
+}
