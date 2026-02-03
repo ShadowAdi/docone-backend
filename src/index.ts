0deb9566bef8987591a90b4079c159818a5f1559
ShadowAdi <shadowshukla76@gmail.com>
2026-02-04 04:12:43 +0530

    use word-extractor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { TextExtraction } from "./utils/textExtraction";
 
-TextExtraction("./src/data/test1.doc").then((result) => {
+TextExtraction("./src/data/test.docx").then((result) => {
     console.log("result ", result)
 }).catch((error) => {
     console.log(`Failed to extract text from the file: `, error)
--- src/utils/extract-from-docx.ts
+++ src/utils/extract-from-docx.ts
@@ -1,24 +1,28 @@
-import { logger } from "../config/logger"
-import { AppError } from "./AppError"
-import fs from "fs";
-import { extractRawText } from "mammoth";
+import WordExtractor from "word-extractor";
+import { logger } from "../config/logger";
+import { AppError } from "./AppError";
 
 export const extractFromDocx = async (filePath: string) => {
-    try {
-        const buffer = fs.readFileSync(filePath)
-        const result = await extractRawText({ buffer })
-
-        const lines = result.value.split("\n")
-
-        return lines.filter((line) => line.trim().length > 0).map((text, i) => ({
-            id: `line_${i}`,
-            text: text.trim(),
-            type: 'line' as const
-        }))
-
-    } catch (error) {
-        logger.error(`Faied to extract docx from the given file path: ${filePath}`)
-        console.error(`Faied to extract docx from the given file path: ${filePath}`)
-        throw new AppError(`Faied to extract docx from the given file path: ${filePath}`, 500)
-    }
-}
+  try {
+    const extractor = new WordExtractor();
+    const extracted = await extractor.extract(filePath);
+    const text = extracted.getBody();
+    const lines = text.split("\n");
+    return lines
+      .filter((line) => line.trim().length > 0)
+      .map((text, i) => ({
+        id: `line_${i}`,
+        text: text.trim(),
+        type: "line" as const,
+      }));
+  } catch (error) {
+    logger.error(`Faied to extract docx from the given file path: ${filePath}`);
+    console.error(
+      `Faied to extract docx from the given file path: ${filePath}`,
+    );
+    throw new AppError(
+      `Faied to extract docx from the given file path: ${filePath}`,
+      500,
+    );
+  }
+};
