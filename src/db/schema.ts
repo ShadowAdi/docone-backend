8132de58a7d34dc19a7d193ec571d7289b4759f8
ShadowAdi <shadowshukla76@gmail.com>
2026-02-04 01:20:25 +0530

    added schema

---
+++ src/db/schema.ts
@@ -0,0 +1,12 @@
+import { uuid, text, integer } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable('users', {
+  id: uuid().defaultRandom().primaryKey(),
+  name: text('name').notNull(),
+  age: integer('age').notNull(),
+  email: text('email').notNull().unique(),
+});
+
+export type InsertUser=typeof usersTable.$inferInsert
+export type SelectUser=typeof usersTable.$inferSelect
