406d57f7c5bc96d787e1a5cf83ab67e37d276f54
ShadowAdi <shadowshukla76@gmail.com>
2026-02-04 04:08:37 +0530

    fix extract from doc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { TextExtraction } from "./utils/textExtraction";
 
-TextExtraction("./src/data/test1.doc")
+TextExtraction("./src/data/test1.doc").then((result) => {
+    console.log("result ", result)
+}).catch((error) => {
+    console.log(`Failed to extract text from the file: `, error)
+})
--- src/utils/extract-from-doc.ts
+++ src/utils/extract-from-doc.ts
@@ -1,21 +1,19 @@
 import { logger } from "../config/logger"
 import { AppError } from "./AppError"
-import fs from "fs";
-import { extractRawText } from "mammoth";
+import WordExtractor from "word-extractor";
+
 
 export const extractFromDoc = async (filePath: string) => {
     try {
-        const buffer = fs.readFileSync(filePath)
-        const result = await extractRawText({ buffer })
-
-        const lines = result.value.split("\n")
-
+        const extractor = new WordExtractor()
+        const extracted = await extractor.extract(filePath)
+        const text = extracted.getBody()
+        const lines = text.split("\n");
         return lines.filter((line) => line.trim().length > 0).map((text, i) => ({
             id: `line_${i}`,
             text: text.trim(),
             type: 'line' as const
         }))
-
     } catch (error) {
         const actualError = error instanceof Error ? error.message : String(error);
         logger.error(`Failed to extract from .doc file: ${filePath} - Error: ${actualError}`);
