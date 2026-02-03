ccfe1ffce1d96ebe24e13c0d0d81f291113438fb
ShadowAdi <shadowshukla76@gmail.com>
2026-02-04 03:14:12 +0530

    quick fix

--- src/utils/textExtraction.ts
+++ src/utils/textExtraction.ts
@@ -1,9 +1,10 @@
 import { logger } from "../config/logger"
 import { AppError } from "./AppError"
 
-export const TextExtraction=async ()=>{
+export const TextExtraction=async (filePath:string)=>{
     try {
-        
+        logger.info(`The Path of the file is: ${filePath}`)
+        console.info(`The Path of the file is: ${filePath}`)
     } catch (error) {
         logger.error(`Failed To Extract Text from the document ${error}`)
         console.error(`Failed To Extract Text from the document ${error}`)
