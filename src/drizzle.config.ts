2c7599a1da18ea3a13b24112831fabcb4727e9d1
ShadowAdi <shadowshukla76@gmail.com>
2026-02-04 01:23:09 +0530

    aaded drizzle config

---
+++ src/drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'drizzle-kit';
+import { SUPABASE_PROJECT_API_URL } from './config/dotenv';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './supabase/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: SUPABASE_PROJECT_API_URL!,
+  },
+});
