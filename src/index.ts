7f9fde748ee4c8dd12969cc08ec2da56fde16d41
ShadowAdi <shadowshukla76@gmail.com>
2026-02-04 10:43:58 +0530

    samll fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { textTranslate } from "./utils/text-translate";
 import { TextExtraction } from "./utils/textExtraction";
 
-TextExtraction("./src/data/test3.pptx").then((result) => {
+TextExtraction("./src/data/test.docx").then((result) => {
     console.log("result ", result)
     result.map((r) => {
         console.log(textTranslate(r.text))
